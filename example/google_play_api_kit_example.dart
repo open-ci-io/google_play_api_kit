c7070c1d14e78779d1a8ed4e2ee5fc5f4ea4ad4b
Masahiro Aoki <freud427@gmail.com>
2023-11-08 12:24:17 +0000

    initial commit

---
+++ example/google_play_api_kit_example.dart
@@ -0,0 +1,6 @@
+import 'package:google_play_api_kit/google_play_api_kit.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/google_play_api_kit_test.dart
@@ -0,0 +1,16 @@
+import 'package:google_play_api_kit/google_play_api_kit.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
